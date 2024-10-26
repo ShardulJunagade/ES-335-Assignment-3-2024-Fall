9b751828de6decec40f66c62c70d244bc414749b
ShardulJunagade <shardul.junagade@gmail.com>
2024-10-26 08:23:28 +0530

    task-3 done, TODO - subjective ans, md file

--- Task-3 MNIST Classification/load_datasets.py
+++ Task-3 MNIST Classification/load_datasets.py
@@ -9,15 +9,43 @@ torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 
 
-def load_mnist_data():
+def load_mnist():
     # define a transform to normalize the data
     transform = transforms.Compose([transforms.ToTensor()])
     # download and load the training data
-    trainset = datasets.MNIST('../mnist_dataset', download=True, train=True, transform=transform)
+    trainset = datasets.MNIST('../datasets', download=True, train=True, transform=transform)
     # trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
 
     # download and load the test data
-    testset = datasets.MNIST('../mnist_dataset', download=True, train=False, transform=transform)
+    testset = datasets.MNIST('../datasets', download=True, train=False, transform=transform)
+    # testloader = DataLoader(testset, batch_size=64, shuffle=False)
+
+    return trainset, testset
+
+
+def load_fasion_mnist():
+    # define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor()])
+    # download and load the training data
+    trainset = datasets.FashionMNIST('../datasets', download=True, train=True, transform=transform)
+    # trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+    # download and load the test data
+    testset = datasets.FashionMNIST('../datasets', download=True, train=False, transform=transform)
+    # testloader = DataLoader(testset, batch_size=64, shuffle=False)
+
+    return trainset, testset
+
+
+def load_cifar10():
+    # define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor()])
+    # download and load the training data
+    trainset = datasets.CIFAR10('../datasets', download=True, train=True, transform=transform)
+    # trainloader = DataLoader(trainset, batch_size=64, shuffle=True)
+
+    # download and load the test data
+    testset = datasets.CIFAR10('../datasets', download=True, train=False, transform=transform)
     # testloader = DataLoader(testset, batch_size=64, shuffle=False)
 
     return trainset, testset
