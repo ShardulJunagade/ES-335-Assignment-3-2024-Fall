d6f65f0a7d0c5ac79ddc16a71be5e97ca32fd7e1
ShardulJunagade <shardul.junagade@gmail.com>
2024-10-29 19:52:20 +0530

    task-2 and 3 compile

---
+++ Task-3 MNIST Classification/load_datasets.py
@@ -0,0 +1,40 @@
+import torch
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Dataset
+
+seed = 0
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+
+def load_mnist():
+    # define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor()])
+    # download and load the training data
+    trainset = datasets.MNIST('../datasets', download=True, train=True, transform=transform)
+    # download and load the test data
+    testset = datasets.MNIST('../datasets', download=True, train=False, transform=transform)
+
+    return trainset, testset
+
+
+def load_fasion_mnist():
+    # define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor()])
+    # download and load the training data
+    trainset = datasets.FashionMNIST('../datasets', download=True, train=True, transform=transform)
+    # download and load the test data
+    testset = datasets.FashionMNIST('../datasets', download=True, train=False, transform=transform)
+
+    return trainset, testset
+
+
+def load_cifar10():
+    # define a transform to normalize the data
+    transform = transforms.Compose([transforms.ToTensor()])
+    # download and load the training data
+    trainset = datasets.CIFAR10('../datasets', download=True, train=True, transform=transform)
+    # download and load the test data
+    testset = datasets.CIFAR10('../datasets', download=True, train=False, transform=transform)
+
+    return trainset, testset
